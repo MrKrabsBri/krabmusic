642d06bde31eb0ef016640843d36d58c8ee8b744
MrKrabsBri <julius.bri@outlook.com>
2024-04-08 12:48:50 +0300

    feat: front-end responsive to GET request

--- krabmusicclient/src/components/Song.js
+++ krabmusicclient/src/components/Song.js
@@ -1,19 +1,58 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import {useState }from 'react';
+import Button from '@mui/material/Button';
 
 export default function Song() {
+
+    const paperStyle = {padding: '50px 20px', width:600, margin: "20px auto"}
+    const [songName,setSongName] = useState('Krab')
+    const [songDescription,setSongDescription]=useState('')
+
+    const handleClick = (e)=>{
+    e.preventDefault()
+    const song={songName,songDescription}
+    console.log(song)
+    fetch("http://localhost:8082/songs",{
+    method:"POST",
+    headers:{"Content-Type":"application/json"},
+    body:JSON.stringify(song)
+    }).then(()=> {
+    console.log("New Song added")
+    })
+    }
+
   return (
     <Box
       component="form"
       sx={{
-        '& > :not(style)': { m: 2, width: '25ch' },
+        '& > :not(style)': { m: 3, width: '25ch' },
       }}
       noValidate
       autoComplete="off"
     >
-      <TextField id="outlined-basic" label="Song Name" variant="outlined" />
-      <TextField id="outlined-basic" label="Song Description" variant="outlined" />
+    <Container>
+        <Paper elevation={5} style={paperStyle}>
+                <h1 style={{color:"green"}}>  <u>Add Song</u>  </h1>
+
+
+                         <TextField id="outlined-basic" label="Song Name" variant="outlined" fullWidth
+                         value = {songName}
+                         onChange={(e)=>setSongName(e.target.value)}
+                         />
+                         <TextField id="outlined-basic" label="Song Description" variant="outlined" fullWidth
+                         value = {songDescription}
+                         onChange={(e)=>setSongDescription(e.target.value)}
+                         />
+                         <Button variant="contained" color= "secondary" onClick={handleClick}>
+                            Submit
+                         </Button>
+
+        </Paper>
+    </Container>
 
     </Box>
   );
